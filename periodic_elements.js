87dd5610267a527e4e754c9f0932fd15133bea13
UnknownGGTime <chayaphatz2556@gmail.com>
2025-11-22 17:02:19 +0700

    Create periodic_elements.js

---
+++ periodic_elements.js
@@ -0,0 +1,13 @@
+
+addIngredient("hydrogen",{
+  color:"#d7f9ff",
+  shape:"gas",
+  group:"gas",
+  type:"gas",
+});
+addIngredient("helium",{
+  color:"#cbcbcb",
+  shape:"gas",
+  group:"gas",
+  type:"gas",
+});
